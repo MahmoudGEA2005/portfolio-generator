9302d5ad13ed474a89e7ba4083498af98b830809
MahmoudGEA2005 <mahmoud.ea2005@gmail.com>
2025-01-26 18:06:46 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/portfolio-generator/", // Replace <repo> with the name of your GitHub repository
+  base: "/portfolio-generator/", // Replace <repo> with the name of your GitHub repository
 });
