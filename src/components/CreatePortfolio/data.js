c35225aa1c2dcb97426dcf86ddb24fba711fcb2e
MahmoudGEA2005 <mahmoud.ea2005@gmail.com>
2025-01-26 14:38:53 +0300

    split data

---
+++ src/components/CreatePortfolio/data.js
@@ -0,0 +1,92 @@
+export const formFields = [
+  {
+    name: "name",
+    type: "text",
+    labelName: "Full Name",
+  },
+  {
+    name: "job_title",
+    type: "text",
+    labelName: "Job Title",
+  },
+  {
+    name: "job_title_logo",
+    type: "text",
+    labelName: "Job Title Logo",
+  },
+  {
+    name: "cv_link",
+    type: "text",
+    labelName: "CV Link",
+  },
+  {
+    name: "email",
+    type: "email",
+    labelName: "Email Address",
+  },
+  {
+    name: "about",
+    type: "textarea",
+    labelName: "About you",
+  },
+  {
+    name: "card1title",
+    type: "text",
+    labelName: "First Card's Title",
+  },
+  {
+    name: "card1info",
+    type: "text",
+    labelName: "First Card's Info",
+  },
+  {
+    name: "card2title",
+    type: "text",
+    labelName: "Second Card's Title",
+  },
+  {
+    name: "card2info",
+    type: "text",
+    labelName: "Second Card's Info",
+  },
+  {
+    name: "aboutlinkonelabel",
+    type: "text",
+    labelName: "First About Link's Label",
+  },
+  {
+    name: "aboutlinkone",
+    type: "text",
+    labelName: "First About Link",
+  },
+  {
+    name: "aboutlinktwolabel",
+    type: "text",
+    labelName: "Second About Link Label",
+  },
+  {
+    name: "aboutlinktwo",
+    type: "text",
+    labelName: "Second About Link",
+  },
+  {
+    name: "github",
+    type: "text",
+    labelName: "GitHub Link",
+  },
+  {
+    name: "youtube",
+    type: "text",
+    labelName: "YouTube Channel",
+  },
+  {
+    name: "linkedin",
+    type: "text",
+    labelName: "Linked In Profile",
+  },
+  {
+    name: "picture",
+    type: "file",
+    labelName: "Your picture",
+  },
+];
